906fc6ce7cd091bc882dde8586ada10ce7ee44d0
Lachlan Campbell <lachlan@hackclub.com>
2020-05-02 15:50:57 -0400

    Add homepage

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -12,8 +12,13 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/" page={HomePage} name="home" />
       <Route path="/recipes/new" page={NewRecipePage} name="newRecipe" />
-      <Route path="/recipes/{id:Int}/edit" page={EditRecipePage} name="editRecipe" />
+      <Route
+        path="/recipes/{id:Int}/edit"
+        page={EditRecipePage}
+        name="editRecipe"
+      />
       <Route path="/recipes/{id:Int}" page={RecipePage} name="recipe" />
       <Route path="/recipes" page={RecipesPage} name="recipes" />
       <Route notfound page={NotFoundPage} />
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <main>
+      <h1>Noodles 3</h1>
+      <p>Keep, cook, & share your favorite recipes. Now built with Redwood.</p>
+    </main>
+  )
+}
+
+export default HomePage
