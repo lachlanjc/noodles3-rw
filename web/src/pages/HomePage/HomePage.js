6ea715ed1c1432ffd43b95b189e4af0b86f69c1c
Lachlan Campbell <lachlan@hackclub.com>
2020-05-03 01:39:39 -0400

    Add link to HomePage

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,8 +1,11 @@
+import { Link, routes } from '@redwoodjs/router'
+
 const HomePage = () => {
   return (
     <main>
       <h1>Noodles 3</h1>
       <p>Keep, cook, & share your favorite recipes. Now built with Redwood.</p>
+      <p><Link to={routes.recipes()}>Open Recipes</Link></p>
     </main>
   )
 }
